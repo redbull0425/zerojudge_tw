5404c36b33394ed1df6caf16b8a20cf451b93ee3
redbull0425 <wuda.idv@gmail.com>
2022-05-23 11:14:14 +0800

    Update a001.c

--- a001/a001.c
+++ a001/a001.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>  //引用字串函式庫
 
 int main() {
-  char W[20];
+  char W[20];  //宣告字串，長度預訂20字元
   
   //printf("Please enter `world`:");
-  scanf("%s", W);
+  scanf("%s", W); //讀入前端輸入，留意字串變數不必加&。字串變數已有指標
   //fgets(W, 20, stdin);
   printf("hello, %s\n", W);
 }
