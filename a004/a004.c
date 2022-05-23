ca9a7cabeb583f46d51af14440989564de55b92f
redbull0425 <wuda.idv@gmail.com>
2022-05-23 06:02:21 +0000

    a004 done

--- a004/a004.c
+++ a004/a004.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main () {
-  int N;
-  print();
+
+int InputNumber;
+    //EOF是在stdio函示庫內，返回值是-1
+    while(scanf("%d",&InputNumber)!=EOF){
+        if(InputNumber%400==0)
+            {
+                printf("閏年\n");
+            }
+        else if(InputNumber%4==0)
+            {
+                if(InputNumber%100!=0)
+                {
+                    printf("閏年\n");
+                }
+                else
+                {
+                    printf("平年\n");
+                }
+            }
+        else
+        {
+            printf("平年\n");
+        }
+    }
+
+
+    return 0;
 }
