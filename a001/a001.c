38ac4d09175c44a9b2586eb4f853f55b10a41abf
redbull0425 <wuda.idv@gmail.com>
2022-05-23 09:36:20 +0800

    first commit

---
+++ a001/a001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char W[20];
+  
+  //printf("Please enter `world`:");
+  scanf("%s", W);
+  //fgets(W, 20, stdin);
+  printf("hello, %s\n", W);
+}
---
+++ a002/a002.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+  //char W[99];
+  int N1, N2;
+  int sum;
+  scanf("%d %d", &N1, &N2);
+  //printf("N1: %d, N2: %d\n", N1, N2);
+  sum = N1 + N2;
+  printf("SUM: %d\n", sum);
+  return 0;
+  
+}
