6265c4857ecc868b658790a7e0969bba4da656c1
redbull0425 <wuda.idv@gmail.com>
2022-05-23 11:27:00 +0800

    Update a002.c

--- a002/a002.c
+++ a002/a002.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
 int main() {
-  //char W[99];
+
   int N1, N2;
   int sum;
-  scanf("%d %d", &N1, &N2);
+  
+  scanf("%d %d", &N1, &N2);  //利用scanf讀入兩數字
   //printf("N1: %d, N2: %d\n", N1, N2);
+  
   sum = N1 + N2;
   printf("SUM: %d\n", sum);
   return 0;
