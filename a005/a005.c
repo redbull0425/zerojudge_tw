dd634c6b086bffe97fb855e31e4aed1adc5696b8
redbull0425 <wuda.idv@gmail.com>
2022-05-24 10:08:32 +0800

    Update a005.c

--- a005/a005.c
+++ a005/a005.c
@@ -2,22 +2,19 @@
 
 int main() {
   int row_n;
-  int R1[5]; //利用陣列
-  int R2[5]; //利用陣列
-  int odd_n; //等差數列
-  int rate_n; //等比數列
+  int R[5]; //利用陣列
 
   scanf("%d", &row_n);
   //printf("%d\n", row_n);
-  scanf("%d %d %d %d", &R1[1], &R1[2], &R1[3], &R1[4]);
-  odd_n=R1[2]-R1[1];
-  scanf("%d %d %d %d", &R2[1], &R2[2], &R2[3], &R2[4]);
-  rate_n=R2[2]/R2[1];
+  for(int i=0; i<row_n; i++) {
+    scanf("%d %d %d %d", &R[1], &R[2], &R[3], &R[4]);
+    printf("%d %d %d %d ", R[1] ,R[2] ,R[3] ,R[4]);
+    if(R[2]-R[1] == R[3]-R[2]) {
+      printf("%d\n", R[4]+(R[2]-R[1]));
+    } else {
+      printf("%d\n", R[4]*(R[2]/R[1]));
+    }
+  }
 
-  //printf("%d\n", odd_n);
-  //printf("%d\n", rate_n);
-
-  printf("%d %d %d %d %d\n", R1[1] ,R1[2] ,R1[3] ,R1[4] ,R1[4]+odd_n);
-  printf("%d %d %d %d %d\n", R2[1] ,R2[2] ,R2[3] ,R2[4] ,R2[4]*rate_n);
   return 0;
 }
