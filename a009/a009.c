c66a235ffe810332cb5ce2f2c4a9fbb9d613d933
redbull0425 <wuda.idv@gmail.com>
2022-06-24 15:56:41 +0800

    update

--- a009/a009.c
+++ a009/a009.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
-#include <math.h>
+int main(){
+/*
+    char str_example[100] = "This is a book.";
 
-int main () {
-  short i=32767;
-  short o1, o2, o3;
-  o1 = i + 1;
-  o2 = i + 2;
-  o3 = i + 32768;
-  printf("32767 overflow status: %d\n", i);
-  printf("\n");
-  printf("32767 + 1 overflow status: %d\n", o1);
-  printf("32767 + 2 overflow status: %d\n", o2);
-  printf("32767 + 32768 overflow status: %d\n", o3);
-  printf("\n");
-  printf("%d\n", 2*2*2*2*2*2*2*2*2*2*2*2*2*2*2);
-  printf("%d\n", (int)pow(2, 15));
-  return 0;
+    for(int n = 0; str_example[n] != '\0'; ++n)  
+      printf(" %d ", str_example[n]);
+    printf("\n");
+*/
+  char str_in[100] = "1JKJ'pz'{ol'{yhklthyr'vm'{ol'Jvu{yvs'Kh{h'Jvywvyh{pvu5";
+  char str_out[100] = "*CDC is the trademark of the Control Data Corporation.";
+
+/*
+  for(int n = 0; str_in[n] != '\0'; ++n) {  
+    printf("%d", str_in[n]);
+  }
+    printf("\n");
+  for(int o = 0; str_out[o] != '\0'; ++o) {  
+    printf("%d", str_out[o]);
+  }
+    printf("\n");
+  for(int m = 0; str_in[m] != '\0'; ++m) {  
+    printf("%c", str_in[m]-7);
+  }
+    printf("\n");
+*/
+    char str[100];
+    scanf("%s", str);    
+    
+    for(int i = 0; str[i] != '\0'; ++i) {  
+        printf("%c", str[i] - 7);
+    }
+        printf("\n");
+/*
+    for(int j = 0; str[j] != '\0'; ++j) { 
+        printf("%d", str[j] - 7);
+    }
+        printf("\n");
+*/
+
+    return 0;
 }
